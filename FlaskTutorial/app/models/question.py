305f39317c32ad1525c835d98d583d83f4808c70
seoyh1618 <seoyh1618@gmail.com>
2024-04-16 05:32:16 +0900

    model update

--- FlaskTutorial/app/models/question.py
+++ FlaskTutorial/app/models/question.py
@@ -4,4 +4,8 @@ from app import db
 
 class Question(db.Model) : 
     id = db.Column(db.Integer,primary_key=True)
-    pass
+    subject = db.Column(db.String(200),nullable = False )
+    content = db.Column(db.Text(),nullable=False)
+    createDate = db.Column(db.DateTime(), nullable = False)
+
+    
